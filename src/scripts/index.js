c4c6bdb8744b4c2c2cb93685c1850e64964810cf
yudistiraardi <mail.ardix@gmail.com>
2023-12-07 01:12:10 +0700

    Creatu layouts

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+import 'regenerator-runtime'; /* for async await transpile */
+
+// Import our custom CSS
+import '../scss/styles.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
+console.log(window.innerHeight);
