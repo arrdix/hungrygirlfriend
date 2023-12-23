3cc9b9089e3b0232d0bf044c1b3dab299ecd355f
yudistiraardi <mail.ardix@gmail.com>
2023-12-23 16:02:32 +0700

    Refactor renderBox, renderStars & renderTeg on Restaurant

--- src/scripts/sections/Restaurant.js
+++ src/scripts/sections/Restaurant.js
@@ -21,92 +21,50 @@ export const Restaurant = {
       const box = document.createElement('div');
       box.classList.add('box', `box-${boxId}`, `resId-${restaurant.id}`);
 
-      if (boxId <= 3) {
-        box.innerHTML = `
-          <div class="action-overlay big">
-            <h3 class="box-title">${restaurant.name} | ${
-          restaurant.rating
-        } <i class="star fa-solid fa-star"></i></h3>
-            <button type="button" aria-label="show ${restaurant.name} detail">
-              <i class="full-screen fa-solid fa-expand"></i>
-            </button>
-          </div>
-          <button type="button" aria-label="show ${restaurant.name} detail" class="box-overlay big">
-            <div class="box-tag">
-              <p>Recommended</p>
-            </div>
-            <h3 class="box-title">${restaurant.name}</h3>
-            <h4 class="box-city">${restaurant.city}</h4>
-            <div class="box-star-wrapper">
-              ${renderStars(roundedRating)}
-            </div>
+      box.innerHTML = `
+        <div class="action-overlay ${boxId <= 3 ? 'big' : ''}">
+          <h3 class="box-title">${restaurant.name} | ${
+        restaurant.rating
+      } <i class="star fa-solid fa-star"></i></h3>
+          <button type="button" aria-label="show ${restaurant.name} detail">
+            <i class="full-screen fa-solid fa-expand"></i>
           </button>
-          <img src="${restaurant.pictureId}" class="restaurant-image" alt="${restaurant.name}">
-        `;
-      } else {
-        box.innerHTML = `
-          <div class="action-overlay">
-            <h3 class="box-title">${restaurant.name} | ${
-          restaurant.rating
-        } <i class="star fa-solid fa-star"></i></h3>
-            <button type="button" aria-label="show ${restaurant.name} detail">
-              <i class="full-screen fa-solid fa-expand"></i>
-            </button>
+        </div>
+        <button type="button" aria-label="show ${restaurant.name} detail" class="box-overlay ${
+        boxId <= 3 ? 'big' : ''
+      }">
+          ${boxId <= 3 ? this.renderTag() : ''}
+          <h3 class="box-title">${restaurant.name}</h3>
+          <h4 class="box-city">${restaurant.city}</h4>
+          <div class="box-star-wrapper">
+            ${this.renderStars(roundedRating)}
           </div>
-          <button type="button" aria-label="show ${restaurant.name} detail" class="box-overlay">
-            <h3 class="box-title">${restaurant.name}</h3>
-            <h4 class="box-city">${restaurant.city}</h4>
-            <div class="box-star-wrapper">
-              ${renderStars(roundedRating)}
-            </div>
-          </button>
-          <img src="${restaurant.pictureId}" class="restaurant-image" alt="${restaurant.name}"">
-        `;
-      }
+        </button>
+        <img src="${restaurant.pictureId}" class="restaurant-image" alt="${restaurant.name}">
+      `;
 
       const boxWrapper = document.getElementById('box-wrapper');
       boxWrapper.appendChild(box);
       boxId++;
-
-      function renderStars(roundedRating) {
-        if (roundedRating === 3.5) {
-          return `
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star-half-stroke"></i>
-          `;
-        }
-
-        if (roundedRating === 4) {
-          return `
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-          `;
-        }
-
-        if (roundedRating === 4.5) {
-          return `
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star-half-stroke"></i>
-          `;
-        }
-
-        if (roundedRating === 5) {
-          return `
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-          `;
-        }
-      }
     });
+  },
+
+  renderStars(roundedRating) {
+    let stars = [];
+    for (let i = 1; i <= roundedRating; i++) {
+      stars.push('<i class="fa-solid fa-star"></i>');
+    }
+
+    if (!(roundedRating % 1 === 0)) stars.push('<i class="fa-solid fa-star-half-stroke"></i>');
+
+    return stars.join('');
+  },
+
+  renderTag() {
+    return `
+    <div class="box-tag">
+      <p>Recommended</p>
+    </div>
+    `;
   },
 };
