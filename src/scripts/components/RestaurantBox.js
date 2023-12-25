07d3584de48f86ccb00149af231e241772d3e02a
yudistiraardi <mail.ardix@gmail.com>
2023-12-25 22:05:12 +0700

    Implement RestaurantBox custom element

---
+++ src/scripts/components/RestaurantBox.js
@@ -0,0 +1,67 @@
+import API_ENDPOINT from '../globals/api-endpoint';
+
+class RestaurantBox extends HTMLElement {
+  set restaurant({ restaurant, recommendedRestaurant, roundedRating }) {
+    this._restaurant = restaurant;
+    this._recommendedRestaurant = recommendedRestaurant;
+    this._roundedRating = roundedRating;
+
+    this.renderBox();
+  }
+
+  renderBox() {
+    this.innerHTML = `
+    <div class="action-overlay ${
+      this._recommendedRestaurant.includes(this._restaurant.id) ? 'big' : ''
+    }">
+      <h3 class="box-title">${this._restaurant.name} | ${
+        this._restaurant.rating
+      } <i class="star fa-solid fa-star"></i></h3>
+      <button type="button" aria-label="show ${this._restaurant.name} detail">
+        <i class="full-screen fa-solid fa-expand"></i>
+      </button>
+    </div>
+    <button type="button" aria-label="show ${
+      this._restaurant.name
+    } detail" class="box-overlay ${
+      this._recommendedRestaurant.includes(this._restaurant.id) ? 'big' : ''
+    }">
+      ${
+        this._recommendedRestaurant.includes(this._restaurant.id)
+          ? this.renderTag()
+          : ''
+      }
+      <h3 class="box-title">${this._restaurant.name}</h3>
+      <h4 class="box-city">${this._restaurant.city}</h4>
+      <div class="box-star-wrapper">
+        ${this.renderStars(this._roundedRating)}
+      </div>
+    </button>
+    <img src="${API_ENDPOINT.IMAGE_LARGE(
+      this._restaurant.pictureId,
+    )}" class="restaurant-image">
+    `;
+  }
+
+  renderStars() {
+    const stars = [];
+    for (let i = 1; i <= this._roundedRating; i++) {
+      stars.push('<i class="fa-solid fa-star"></i>');
+    }
+
+    if (!(this._roundedRating % 1 === 0))
+      stars.push('<i class="fa-solid fa-star-half-stroke"></i>');
+
+    return stars.join('');
+  }
+
+  renderTag() {
+    return `
+    <div class="box-tag">
+      <p>Recommended</p>
+    </div>
+    `;
+  }
+}
+
+customElements.define('restaurant-box', RestaurantBox);
--- src/scripts/sections/Restaurant.js
+++ src/scripts/sections/Restaurant.js
@@ -1,69 +1,57 @@
 import RestaurantSource from '../data/restaurant-source';
-import API_ENDPOINT from '../globals/api-endpoint';
+import '../components/RestaurantBox';
 
 const Restaurant = {
   init() {
-    this.renderBox();
+    this.getRestaurant();
   },
 
-  async renderBox() {
+  async getRestaurant() {
     const restaurants = await RestaurantSource.restaurantList();
+    this.findRecommendedRestaurant(restaurants);
+  },
+
+  async findRecommendedRestaurant(restaurants) {
     const recommendedRestaurant = [];
-    let recommendedId = 1;
-    let boxId = 1;
+    let recommendedId = 0;
 
     restaurants.forEach((restaurant) => {
       const roundedRating = Math.floor(restaurant.rating / 0.5) * 0.5;
-      const boxElement = document.createElement('div');
 
       if (this.isRecommended(recommendedRestaurant, roundedRating)) {
         recommendedRestaurant.push(restaurant.id);
+        recommendedId++;
       }
 
-      boxElement.setAttribute('id', `${restaurant.id}`);
-      boxElement.classList.add(
-        'box',
-        `${
-          recommendedRestaurant.includes(restaurant.id)
-            ? `big-box-${recommendedId++}`
-            : `box-${boxId++}`
-        }`,
-      );
-      boxElement.innerHTML = `
-        <div class="action-overlay ${
-          recommendedRestaurant.includes(restaurant.id) ? 'big' : ''
-        }">
-          <h3 class="box-title">${restaurant.name} | ${
-            restaurant.rating
-          } <i class="star fa-solid fa-star"></i></h3>
-          <button type="button" aria-label="show ${restaurant.name} detail">
-            <i class="full-screen fa-solid fa-expand"></i>
-          </button>
-        </div>
-        <button type="button" aria-label="show ${
-          restaurant.name
-        } detail" class="box-overlay ${
-          recommendedRestaurant.includes(restaurant.id) ? 'big' : ''
-        }">
-          ${
-            recommendedRestaurant.includes(restaurant.id)
-              ? this.renderTag()
-              : ''
-          }
-          <h3 class="box-title">${restaurant.name}</h3>
-          <h4 class="box-city">${restaurant.city}</h4>
-          <div class="box-star-wrapper">
-            ${this.renderStars(roundedRating)}
-          </div>
-        </button>
-        <img src="${API_ENDPOINT.IMAGE_LARGE(
-          restaurant.pictureId,
-        )}" class="restaurant-image" alt="${restaurant.name}">
-      `;
-
-      const boxWrapper = document.getElementById('box-wrapper');
-      boxWrapper.appendChild(boxElement);
+      this.createRestaurantBox({
+        restaurant,
+        recommendedRestaurant,
+        recommendedId,
+      });
     });
+  },
+
+  createRestaurantBox({ restaurant, recommendedRestaurant, recommendedId }) {
+    const roundedRating = Math.floor(restaurant.rating / 0.5) * 0.5;
+    const restaurantBox = document.createElement('restaurant-box');
+
+    restaurantBox.setAttribute('id', `${restaurant.id}`);
+    restaurantBox.classList.add(
+      'box',
+      `${
+        recommendedRestaurant.includes(restaurant.id)
+          ? `big-box-${recommendedId}`
+          : 'normal-box'
+      }`,
+    );
+    restaurantBox.restaurant = {
+      restaurant,
+      recommendedRestaurant,
+      roundedRating,
+    };
+
+    const boxWrapper = document.getElementById('box-wrapper');
+    boxWrapper.appendChild(restaurantBox);
   },
 
   isRecommended(recommendedRestaurant, rating) {
@@ -74,26 +62,6 @@ const Restaurant = {
     }
 
     return false;
-  },
-
-  renderStars(roundedRating) {
-    const stars = [];
-    for (let i = 1; i <= roundedRating; i++) {
-      stars.push('<i class="fa-solid fa-star"></i>');
-    }
-
-    if (!(roundedRating % 1 === 0))
-      stars.push('<i class="fa-solid fa-star-half-stroke"></i>');
-
-    return stars.join('');
-  },
-
-  renderTag() {
-    return `
-    <div class="box-tag">
-      <p>Recommended</p>
-    </div>
-    `;
   },
 };
 
